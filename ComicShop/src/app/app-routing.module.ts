0fb8691f07f2d7f7dd6063357268fa510bb020f3
Guilherme_Endrigo <endrigo.guilherme@hotmail.com>
2021-07-19 21:59:07 -0300

    rota de erro conectada ao principal

--- ComicShop/src/app/app-routing.module.ts
+++ ComicShop/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CharComponent } from './components/char-component/char-component.compon
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { ProdutosComponent } from './components/produtos/produtos.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
 
@@ -30,6 +31,8 @@ const routes: Routes = [
 
   { path: 'contato', component: ContactUsComponent },
 
+  {path: 'not-found', component: PageNotFoundComponent},
+  
   { path:"**", redirectTo: '/not-found' }
   
 ];
